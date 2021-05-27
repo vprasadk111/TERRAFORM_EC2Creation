32d1f3d280a0cdb05dc520d1699715506836e2b5
vprasadk111 <84302211+vprasadk111@users.noreply.github.com>
2021-05-27 15:43:23 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_default_vpc" "default" {
+
+}
+
+
+resource "aws_instance" "terraforminstacne1" {
+  ami           = "ami-0742b4e673072066f"
+  instance_type = "t2.micro"
+  key_name      = "test"
+  tags = {
+    Name = "terraforminstacne1"
+
+  }
+}
