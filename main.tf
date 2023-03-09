f66a8dd5980aebf10c61be400da8b28c9312d096
vprasadk111 <84302211+vprasadk111@users.noreply.github.com>
2023-03-09 18:20:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_default_vpc" "default" {
 
 
 resource "aws_instance" "terraforminstacne1" {
-  ami           = "ami-0742b4e673072066f"
+  #ami           = "ami-0742b4e673072066f"
   instance_type = "t2.micro"
   key_name      = "testkey"
   tags = {
